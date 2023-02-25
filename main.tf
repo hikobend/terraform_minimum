0b8ddbb7d385b6fb5fe26bafb33506090d6e4027
hikobend <hiko.bkend@gmail.com>
2023-02-25 11:28:13 +0900

    vpc subnet internet-gateway

--- main.tf
+++ main.tf
@@ -1,4 +1,80 @@
-resource "aws_instance" "name" {
-  ami = "ami-0ffac3e16de16665e"
-  instance_type = "t2.micro"
+terraform {
+  required_version = "1.3.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+variable title {
+  type = string
+}
+
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  
+  tags = {
+    Name = "${var.title}-vpc"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1a" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.title}-public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1c" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.title}-public-subnet-1c"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1a" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.title}-private-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1c" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.title}-private-subnet-1c"
+  }
+}
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.title}-internet-gateway"
+  }
 }
